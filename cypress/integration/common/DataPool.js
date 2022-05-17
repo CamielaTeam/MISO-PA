62132141310014156d6b7f29483f1849aa489dd4
Camilo Sanchez <ca.sanchezc12@uniandes.edu.co>
2022-05-16 21:13:26 -0500

    Configurar escenarios pseudoaleatorios

--- cypress/integration/common/DataPool.js
+++ cypress/integration/common/DataPool.js
@@ -1,3 +1,5 @@
+const { faker } = require("@faker-js/faker");
+
 export const dataDict = {
   "a correct email": "test5@example.com",
   "a correct password": "password123",
@@ -9,4 +11,16 @@ export const dataDict = {
   "an empty text": " ",
   "an email with whitespace at the end": "test_email@example.com ",
   "an email with whitespace": "test email@example.com",
+};
+
+export const pseudoRandDict = {
+  "a correct email": faker.internet.email(),
+  "a correct password": faker.internet.password(),
+  "a very long password": faker.internet.password(100),
+  "a password with whitespace": faker.internet.password(
+    null,
+    null,
+    null,
+    "                    "
+  ),
 };
